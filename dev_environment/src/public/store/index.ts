2996384d0393626574e52a43d4925bb3e73fd097
gonzaarancibia <gonzaloarancibia48@gmail.com>
2025-03-24 14:59:15 -0300

    refactor(store): remove unused hooks and filter slice

--- dev_environment/src/public/store/index.ts
+++ dev_environment/src/public/store/index.ts
@@ -1,8 +1,6 @@
 import create from 'zustand';
 import { TodoSlice, createTodoSlice } from './slices/todoSlice';
-import { FilterSlice, createFilterSlice } from './slices/filterSlice';
 
-export const useStore = create<TodoSlice & FilterSlice>()((...args) => ({
+export const useStore = create<TodoSlice>()((...args) => ({
     ...createTodoSlice(...args),
-    // ...createFilterSlice(...args),
 }));
